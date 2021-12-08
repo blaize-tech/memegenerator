d96af80a18499f90d578439a516fcf0a733d7510
Vladyslav Valeev <vladyslav.v@blaize.tech>
2021-12-08 10:41:35 +0200

    Initial commit with all code that available. Complete dynamic storage of data on-chain and token mint. Also here commented code with checking token per account (not working yet)

---
+++ contracts/meme-template/src/lib.rs
@@ -0,0 +1,214 @@
+use chrono::prelude::{DateTime, Utc};
+use near_contract_standards::non_fungible_token::core::NonFungibleTokenCore;
+use near_contract_standards::non_fungible_token::{
+    metadata::{NFTContractMetadata, TokenMetadata, NFT_METADATA_SPEC},
+    NonFungibleToken, Token, TokenId,
+};
+use near_sdk::borsh::{self, BorshDeserialize, BorshSerialize};
+use near_sdk::collections::LazyOption;
+use near_sdk::collections::Vector;
+use near_sdk::json_types::{Base64VecU8, ValidAccountId};
+use near_sdk::{env, near_bindgen, BorshStorageKey, PanicOnDefault};
+use sha2::{Digest, Sha256};
+
+near_sdk::setup_alloc!();
+
+const DATA_IMAGE_SVG_NEAR_ICON: &str = "data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 288 288'%3E%3Cg id='l' data-name='l'%3E%3Cpath d='M187.58,79.81l-30.1,44.69a3.2,3.2,0,0,0,4.75,4.2L191.86,103a1.2,1.2,0,0,1,2,.91v80.46a1.2,1.2,0,0,1-2.12.77L102.18,77.93A15.35,15.35,0,0,0,90.47,72.5H87.34A15.34,15.34,0,0,0,72,87.84V201.16A15.34,15.34,0,0,0,87.34,216.5h0a15.35,15.35,0,0,0,13.08-7.31l30.1-44.69a3.2,3.2,0,0,0-4.75-4.2L96.14,186a1.2,1.2,0,0,1-2-.91V104.61a1.2,1.2,0,0,1,2.12-.77l89.55,107.23a15.35,15.35,0,0,0,11.71,5.43h3.13A15.34,15.34,0,0,0,216,201.16V87.84A15.34,15.34,0,0,0,200.66,72.5h0A15.35,15.35,0,0,0,187.58,79.81Z'/%3E%3C/g%3E%3C/svg%3E";
+
+fn iso8601(st: &std::time::SystemTime) -> String {
+    let dt: DateTime<Utc> = st.clone().into();
+    format!("{}", dt.format("%+"))
+}
+
+#[derive(BorshSerialize, BorshStorageKey)]
+enum StorageKey {
+    NonFungibleToken,
+    Metadata,
+    TokenMetadata,
+    Enumeration,
+    Approval,
+}
+
+#[derive(BorshDeserialize, BorshSerialize)]
+pub struct MemeTemplate {
+    nft_id: i32,
+    ntf_metadata: std::string::String,
+    creator: std::string::String, // ValidAccountId for production
+}
+
+#[near_bindgen]
+#[derive(PanicOnDefault, BorshDeserialize, BorshSerialize)]
+pub struct Counter {
+    container: Vector<MemeTemplate>,
+    tokens: NonFungibleToken,
+    metadata: LazyOption<NFTContractMetadata>,
+}
+
+#[near_bindgen]
+impl Counter {
+    #[init]
+    pub fn new(owner_id: ValidAccountId) -> Self {
+        Self {
+            container: Vector::new(b"s".to_vec()),
+            tokens: NonFungibleToken::new(
+                StorageKey::NonFungibleToken,
+                owner_id,
+                Some(StorageKey::TokenMetadata),
+                Some(StorageKey::Enumeration),
+                Some(StorageKey::Approval),
+            ),
+            metadata: LazyOption::new(
+                StorageKey::Metadata,
+                Some(&NFTContractMetadata {
+                    spec: NFT_METADATA_SPEC.to_string(),
+                    name: "Example NEAR non-fungible token".to_string(),
+                    symbol: "EXAMPLE".to_string(),
+                    icon: Some(DATA_IMAGE_SVG_NEAR_ICON.to_string()),
+                    base_uri: None,
+                    reference: None,
+                    reference_hash: None,
+                }),
+            ),
+        }
+    }
+
+    #[payable]
+    pub fn nft_mint(
+        &mut self,
+        token_id: TokenId,
+        owner_id: ValidAccountId,
+        token_metadata: TokenMetadata
+    ) -> Token {
+        /*let mut s = std::string::String::from("123");
+        near call $ID nft_mint '{"token_id": "0", "receiver_id": "'$ID'", "token_metadata": { "title": "Olympus Mons", "description": "Tallest mountain in charted solar system", "media": "https://upload.wikimedia.org/wikipedia/commons/thumb/0/00/Olympus_Mons_alt.jpg/1024px-Olympus_Mons_alt.jpg", "copies": 1}}' --accountId $ID --deposit 0.1
+
+        ValidAccountId::try_from()
+        let key = ValidAccountId::try_from("alice.near").unwrap();
+
+        let mut owner_tokens: Vec<Token> =
+            self.tokens.nft_tokens_for_owner(, None, None).unwrap_json();
+
+        let log_message = format!("Tokens = {:?}", owner_tokens);
+        env::log(log_message.as_bytes());*/
+
+        self.tokens.mint(token_id, owner_id, Some(token_metadata))
+    }
+
+    /*#[payable]
+    pub fn nft_mint(
+        &mut self,
+        token_id: TokenId,
+        owner_id: ValidAccountId,
+        title: std::string::String,
+        description: std::string::String,
+        media: std::string::String,
+    ) -> Token {
+        // Generating media hash
+        let mut media_hash = Sha256::new();
+        media_hash.update(media.clone());
+        let media_hash: std::string::String = format!("Media hash: {:X}", media_hash.finalize());
+        let media_hash: Base64VecU8 = Base64VecU8(media_hash.try_to_vec().unwrap());
+
+        self.tokens.mint(
+            token_id,
+            owner_id,
+            Some(TokenMetadata {
+                title: Some(title.into()),
+                description: Some(description.into()),
+                media: Some(media.into()),
+                media_hash: Some(media_hash),
+                copies: Some(1u64),
+                issued_at: Some(iso8601(&std::time::SystemTime::now())),
+                expires_at: None,
+                starts_at: None,
+                updated_at: None,
+                extra: None,
+                reference: None,
+                reference_hash: None,
+            }),
+        )
+    }*/
+
+
+    pub fn increment(&mut self, value: &i32) {
+        // note: adding one like this is an easy way to accidentally overflow
+        // real smart contracts will want to have safety checks
+        // e.g. self.val = i8::wrapping_add(self.val, 1);
+        // https://doc.rust-lang.org/std/primitive.i8.html#method.wrapping_add
+        //self.val += value.to_i8().unwrap();
+        self.container.push(&MemeTemplate {
+            nft_id: *value,
+            ntf_metadata: "".to_string(),
+            creator: "".to_string(),
+        });
+
+        let mut sum = 0;
+        for val in self.container.iter() {
+            sum += val.nft_id;
+        }
+
+        let log_message = format!("Sum = {}", sum);
+        env::log(log_message.as_bytes());
+    }
+}
+
+// use the attribute below for unit tests
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use near_sdk::json_types::ValidAccountId;
+    use near_sdk::serde::export::TryFrom;
+    use near_sdk::test_utils::VMContextBuilder;
+    use near_sdk::testing_env;
+    use near_sdk::MockedBlockchain;
+
+    // simple helper function to take a string literal and return a ValidAccountId
+    fn to_valid_account(account: &str) -> ValidAccountId {
+        ValidAccountId::try_from(account.to_string()).expect("Invalid account")
+    }
+
+    // part of writing unit tests is setting up a mock context
+    // provide a `predecessor` here, it'll modify the default context
+    fn get_context(predecessor: ValidAccountId) -> VMContextBuilder {
+        let mut builder = VMContextBuilder::new();
+        builder.predecessor_account_id(predecessor);
+        builder
+    }
+
+    // mark individual unit tests with #[test] for them to be registered and fired
+    #[test]
+    fn increment() {
+        // set up the mock context into the testing environment
+        let context = get_context(to_valid_account("foo.near"));
+        testing_env!(context.build());
+        // instantiate a contract variable with the counter at zero
+        let mut contract = Counter {};
+        contract.increment(&5);
+        println!("Value after increment: {}", contract.get_num());
+        // confirm that we received 1 when calling get_num
+        assert_eq!(1, contract.get_num());
+    }
+
+    #[test]
+    fn decrement() {
+        let context = VMContextBuilder::new();
+        testing_env!(context.build());
+        let mut contract = Counter {};
+        contract.decrement();
+        println!("Value after decrement: {}", contract.get_num());
+        // confirm that we received -1 when calling get_num
+        assert_eq!(-1, contract.get_num());
+    }
+
+    #[test]
+    fn increment_and_reset() {
+        let context = VMContextBuilder::new();
+        testing_env!(context.build());
+        let mut contract = Counter {};
+        contract.increment(&11);
+        contract.reset();
+        println!("Value after reset: {}", contract.get_num());
+        // confirm that we received -1 when calling get_num
+        assert_eq!(0, contract.get_num());
+    }
+}
